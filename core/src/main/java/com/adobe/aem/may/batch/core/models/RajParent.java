224e5ba0fa01c8f07a6bcbce0b963ba0693c7e62
Rajuyadav127 <kolikonda7755@gamil.com>
2025-06-02 11:31:55 +0530

    maybatch

--- core/src/main/java/com/adobe/aem/may/batch/core/models/RajParent.java
+++ core/src/main/java/com/adobe/aem/may/batch/core/models/RajParent.java
@@ -2,13 +2,20 @@ package com.adobe.aem.may.batch.core.models;
 
 import java.util.List;
 
+import javax.inject.Inject;
+
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ChildResource;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
-@Model(adaptables = Resource.class,defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+import com.day.cq.wcm.api.Page;
+
+@Model(adaptables = {Resource.class,SlingHttpServletRequest.class},defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class RajParent {
 
     @ValueMapValue
@@ -20,6 +27,32 @@ public class RajParent {
     @ValueMapValue
     public int num;
 
+    @ScriptVariable
+    Page currentPage;
+
+    @Inject
+    ResourceResolver resolver;
+
+    public String getArticletitle()
+    {
+        return currentPage.getTitle();
+    }
+    public String getArticlePagePath()
+    {
+        return currentPage.getPath();
+    }
+    public Page getArticlePageParent()
+    {
+        return currentPage.getParent();
+    }
+
+    public String getWebContentNode()
+    {
+        return resolver.getUserID();
+
+    }
+
+    
     @ChildResource
     public List<RajChild> bookdetailswithmap;
 
